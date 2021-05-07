5515feedc393b8fdaa4ff55f086e7d828ab0f569
TonyCastillo <tony.castillo.7@gmail.com>
2021-05-07 11:59:49 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+
+/* $(window).scroll(function(){
+    if ($(this).scrollTop() > 50) {
+       $('#nav-syngenta').addClass('newClass');
+    } else {
+       $('#nav-syngenta').removeClass('newClass');
+    }
+}); */
+let seeker_color = 'assets/img/logo_seeker.png';
+let seeker_blanco = 'assets/img/logo_seeker_white.png';
+let syngenta_color = 'assets/img/logo_syngenta.png';
+let syngenta_blanco = 'assets/img/logo_syngenta_white.png';
+let logo_seeker= document.getElementById('logo_seeker');
+let logo_syngenta= document.getElementById('logo_syngenta');
+
+
+
+console.log(navs_items)
+/* for (let items = 0; items < navs_items.length; items++){
+   navs_items[items].src = `assets/images/ico-menu0${items+1}.png`;
+} */
